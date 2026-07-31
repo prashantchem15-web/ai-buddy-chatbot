459ac81e98e72e94979ce8b766e6c956232e6556
prashantchem15-web <prashantchem15@gmail.com>
2026-07-31 22:33:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from google import genai
+import google.generativeai as genai
 import os
 
 app = Flask(__name__)
 
-client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
-
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+model = genai.GenerativeModel("gemini-3.5-flash-lite")
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -20,10 +19,7 @@ def chat():
         if not data or "message" not in data:
             return jsonify({"reply": "Please enter a message."})
 
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=data["message"]
-        )
+        response = model.generate_content(data["message"])
 
         return jsonify({
             "reply": response.text
@@ -33,7 +29,6 @@ def chat():
         return jsonify({
             "reply": str(e)
         }), 500
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
