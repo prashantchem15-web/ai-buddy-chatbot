e0d68a42f80e672b7ef9e9a1318c72783e2b1394
prashantchem15-web <prashantchem15@gmail.com>
2026-07-31 21:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-client = OpenAI(api_key=os.environ["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"])
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
 @app.route("/")
